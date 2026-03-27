a82da71ec67d438c96795bf1e89a94c3d062807f
anapauladossantosferreira <ana.ferreira.siqueira@escola.pr.gov.br>
2026-03-27 09:38:54 -0300

    .

--- script.js
+++ script.js
@@ -1,84 +1,91 @@
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
-// Configurações dos Carros
+const teclas = {};
+window.addEventListener("keydown", e => teclas[e.key] = true);
+window.addEventListener("keyup", e => teclas[e.key] = false);
+
 class Carro {
     constructor(x, y, cor, controles) {
         this.x = x;
         this.y = y;
-        this.largura = 40;
-        this.altura = 20;
-        this.cor = cor;
+        this.angulo = 0; // Em radianos
         this.velocidade = 0;
-        this.aceleracao = 0.2;
-        this.friccao = 0.05;
-        this.maxVel = 5;
-        this.controles = controles; // { up, down }
+        this.cor = cor;
+        this.controles = controles;
+        
+        // Propriedades físicas
+        this.aceleracao = 0.15;
+        this.friccao = 0.98;
+        this.velGiro = 0.05;
+    }
+
+    atualizar() {
+        // Acelerar / Ré
+        if (teclas[this.controles.up]) this.velocidade += this.aceleracao;
+        if (teclas[this.controles.down]) this.velocidade -= this.aceleracao;
+
+        // Girar (apenas se estiver em movimento)
+        if (Math.abs(this.velocidade) > 0.1) {
+            const direcaoGiro = this.velocidade > 0 ? 1 : -1;
+            if (teclas[this.controles.left]) this.angulo -= this.velGiro * direcaoGiro;
+            if (teclas[this.controles.right]) this.angulo += this.velGiro * direcaoGiro;
+        }
+
+        // Aplicar Física
+        this.velocidade *= this.friccao;
+        this.x += Math.cos(this.angulo) * this.velocidade;
+        this.y += Math.sin(this.angulo) * this.velocidade;
+
+        // Limites da tela (teleporte)
+        if (this.x > canvas.width) this.x = 0;
+        if (this.x < 0) this.x = canvas.width;
+        if (this.y > canvas.height) this.y = 0;
+        if (this.y < 0) this.y = canvas.height;
     }
 
     desenhar() {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angulo);
+        
+        // Corpo do carro
         ctx.fillStyle = this.cor;
-        ctx.fillRect(this.x, this.y, this.largura, this.altura);
-    }
-
-    atualizar(teclas) {
-        // Movimentação
-        if (teclas[this.controles.up]) {
-            this.velocidade += this.aceleracao;
-        } else if (teclas[this.controles.down]) {
-            this.velocidade -= this.aceleracao;
-        }
-
-        // Limite de velocidade e inércia
-        if (this.velocidade > this.maxVel) this.velocidade = this.maxVel;
-        if (this.velocidade < 0) this.velocidade = 0;
-        this.velocidade *= (1 - this.friccao);
-
-        this.x += this.velocidade;
-
-        // Verificar Vitoria
-        if (this.x > canvas.width - this.largura) {
-            alert("O carro " + this.cor + " venceu!");
-            this.resetar();
-        }
-    }
-
-    resetar() {
-        this.x = 20;
-        this.velocidade = 0;
+        ctx.fillRect(-15, -10, 30, 20);
+        
+        // Faróis (para saber a frente)
+        ctx.fillStyle = "yellow";
+        ctx.fillRect(10, -8, 5, 4);
+        ctx.fillRect(10, 4, 5, 4);
+        
+        ctx.restore();
     }
 }
 
-// Instâncias
-const p1 = new Carro(20, 100, "red", { up: "ArrowRight", down: "ArrowLeft" });
-const p2 = new Carro(20, 250, "cyan", { up: "d", down: "a" });
+// Criar Jogadores
+const p1 = new Carro(100, 300, "#ff4757", { up: "ArrowUp", down: "ArrowDown", left: "ArrowLeft", right: "ArrowRight" });
+const p2 = new Carro(100, 350, "#1e90ff", { up: "w", down: "s", left: "a", right: "d" });
 
-const teclas = {};
+function loop() {
+    // Fundo
+    ctx.fillStyle = "#2f3542";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-window.addEventListener("keydown", (e) => teclas[e.key] = true);
-window.addEventListener("keyup", (e) => teclas[e.key] = false);
+    // Desenhar "Pista" simples
+    ctx.strokeStyle = "rgba(255,255,255,0.1)";
+    ctx.lineWidth = 2;
+    for(let i=0; i<canvas.width; i+=50) {
+        ctx.beginPath(); ctx.moveTo(i, 0); ctx.lineTo(i, canvas.height); ctx.stroke();
+        ctx.beginPath(); ctx.moveTo(0, i); ctx.lineTo(canvas.width, i); ctx.stroke();
+    }
 
-// Loop principal
-function gameLoop() {
-    // Limpar tela
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    // Desenhar Linha de Chegada
-    ctx.strokeStyle = "white";
-    ctx.setLineDash([10, 10]);
-    ctx.beginPath();
-    ctx.moveTo(750, 0);
-    ctx.lineTo(750, 400);
-    ctx.stroke();
-
-    // Atualizar e Desenhar Carros
-    p1.atualizar(teclas);
+    p1.atualizar();
     p1.desenhar();
-    
-    p2.atualizar(teclas);
+
+    p2.atualizar();
     p2.desenhar();
 
-    requestAnimationFrame(gameLoop);
+    requestAnimationFrame(loop);
 }
 
-gameLoop();
+loop();
