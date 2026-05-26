ca14835c34ffb75433e44621a2e12a14b1ccdd75
anapauladossantosferreira <ana.ferreira.siqueira@escola.pr.gov.br>
2026-05-26 07:34:11 -0300

    .

--- script.js
+++
@@ -1,3 +0,0 @@
-// Carregar imagens com garantia de erro
-const car1Img = new Image(); car1Img.src = 'COLOQUE_A_URL_DO_CARRO_1_AQUI'; // Carro P1
-const car2Img = new Image(); car2Img.src = 'COLOQUE_A_URL_DO_CARRO_2_AQUI'; // Carro P2
